aa97376663c48957d8c510ce4b9c9555f397bc3f
DilshanDilipudara <gunawardhanaudara@gmail.com>
2021-05-19 07:59:58 +0530

    stack

--- stackOverflower/main.go
+++ stackOverflower/main.go
@@ -3,48 +3,39 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	
+
 )
 
 //User defines model for storing account details in database
 type db_field struct {
-	
-	// office
-	OfficeCode 		string `json:"officeCode,omitempty"`
-	OfficeName 		string `json:"officeName,omitempty"`
-	City 			string `json:"city,omitempty"`
-	AddressLine1  	string `json:"addressLine1,omitempty"`
-	Address 		string `json:"address,omitempty"`
-	AddressLine2 	string `json:"addressLine2,omitempty"`
-	PostalCode 		string `json:"postalCode,omitempty"`
-	Street 			string `json:"street,omitempty"`
-	Country 		string `json:"country,omitempty"`
 
 	// employee
 	EmployeeNumber  int    `json:"employeeNumber,omitempty",`
 	FirstName       string `json:"firstName,omitempty",`
 	LastName        string `json:"lastName,omitempty",`
-	
-	Email  			string `json:"email,omitempty"`
-	JobTitle  		string `json:"job,omitempty",`
-	PhoneNumber     string `json:"phonenumber,omitempty",`
+	Email  			string `json:"email,omitempty",`
+	Name 	 		string `json:"name,omitempty",`
+	Job  		string `json:"job,omitempty",` 
+	Address  		string `json:"address,omitempty",`
 }
 
 func main(){
+	fmt.Println("Schema change apply is up .")
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", changeRequestForUpdatedVersion)
 	
-	http.ListenAndServe(":50002", mux)
+	http.ListenAndServe(":50000", mux)
 }
 
 func changeRequestForUpdatedVersion(w http.ResponseWriter, r *http.Request){
+	
 	log.Println("POST request received from localhost:50000")
 	db := db_field{} //initialize 
 
-	// log.Println(r)
 	//Parse json request body and use it to set fields on db
 	err := json.NewDecoder(r.Body).Decode(&db)
 	
@@ -52,15 +43,14 @@ func changeRequestForUpdatedVersion(w http.ResponseWriter, r *http.Request){
 		panic(err)
 	}
 	log.Println("Apply DB changes Shcema for POST Request")
-	// apply schema changes 
-	db.Address = db.AddressLine1
-	db.AddressLine1 = ""
-	db.AddressLine2 = ""
 	
-	db.PhoneNumber = "0710000000"
-	pc := db.PostalCode  // change shema bind the value
-	db.PostalCode = pc
+	// change body
+	db.Email = ""
+	db.Address = "No.xx, xxx,"
+	db.Name = db.FirstName + db.LastName
+	db.Email = ""
 	
+	fmt.Println(db)
 	//Marshal or convert user object back to json and write to response 
 	dbJson, err := json.Marshal(db)
 	if err != nil{
@@ -78,8 +68,6 @@ func changeRequestForUpdatedVersion(w http.ResponseWriter, r *http.Request){
 		log.Fatalf("An Error Occured %v", err)
 	 }
 	 defer resp.Body.Close()
-
-	
 
 	
 }
